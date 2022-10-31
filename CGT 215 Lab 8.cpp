cd704c3c28d403322d97da592295fa81a23ef7f6
Cameron Thomas Schneider <ctschnei@purdue.edu>
2022-10-31 12:12:42 -0400

    Save Point

--- CGT 215 Lab 8.cpp
+++ CGT 215 Lab 8.cpp
@@ -9,6 +9,12 @@ using namespace sfp;
 
 const float KB_SPEED = 0.2;
 
+void LoadTex(Texture& tex, string filename) {
+	if (!tex.loadFromFile(filename)) {
+		cout << "Can't Load" << filename << endl;
+	}
+}
+
 void MoveCrossbow(PhysicsSprite& crossbow, int elapsedMS) {
 	if (Keyboard::isKeyPressed(Keyboard::Right)) {
 		Vector2f newPos(crossbow.getCenter());
@@ -28,4 +34,23 @@ int main()
 	World world(Vector2f(0, 0));
 	int arrows(5);
 	int score(0);
+
+	PhysicsSprite& crossBow = *new PhysicsSprite();
+	Texture cbowTex;
+	LoadTex (cbowTex, "Lab08Images/crossbow.png");
+	crossBow.setTexture(cbowTex);
+	Vector2f sz = crossBow.getSize();
+	crossBow.setCenter(Vector2f(400, 600 - (sz.y / 2)));
+
+	PhysicsSprite arrow;
+	Texture arrowTex;
+	LaodTex(arrowTex, "Lab08Images/arrow.png");
+	arrow.setTexture(arrowTex);
+	bool drawingArrow(false);
+
+	PhysicsRectangle top;
+	top.setSize(Vector2f(800, 10));
+	top.setCenter(Vector2f(400, 5));
+	top.setStatic(true);
+	world.AddPhysicsBody(top);
 }

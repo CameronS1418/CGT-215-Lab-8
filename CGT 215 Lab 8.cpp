662ddaa70ec932b03963947bfeccb958a5ac0ef3
Cameron Thomas Schneider <ctschnei@purdue.edu>
2022-10-31 11:55:59 -0400

    Save Point

--- CGT 215 Lab 8.cpp
+++ CGT 215 Lab 8.cpp
@@ -1,20 +1,31 @@
-// CGT 215 Lab 8.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFPhysics.h>
+#include <vector>
+
+using namespace std;
+using namespace sf;
+using namespace sfp;
+
+const float KB_SPEED = 0.2;
+
+void MoveCrossbow(PhysicsSprite& crossbow, int elapsedMS) {
+	if (Keyboard::isKeyPressed(Keyboard::Right)) {
+		Vector2f newPos(crossbow.getCenter());
+		newPos.x = newPos.x + (KB_SPEED * elapsedMS);
+		crossbow.setCenter(newPos);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Left)) {
+		Vector2f newPos(crossbow.getCenter());
+		newPos.x = newPos.x - (KB_SPEED * elapsedMS);
+		crossbow.setCenter(newPos);
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	RenderWindow window(VideoMode(800, 600), "Duck Hunting");
+	World world(Vector2f(0, 0));
+	int arrows(5);
+	int score(0);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
